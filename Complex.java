d56643fda3fbcef95fa30b4fe97ec63f030e2dde
mzshd <mashak8dk@gmail.com>
2024-11-19 21:22:53 +0100

    general

--- Complex.java
+++ Complex.java
@@ -2,39 +2,44 @@ public class Complex {
     private double re, im;
 
     public Complex () {
+        // Default costructor for a complex number 0 + 0 i.
         this(0,0);
     }
 
     public Complex (double re, double im) {
+        // Construct a complex number object from two decimal values.
         this.re = re;
         this.im = im;
     }
 
     public Complex (Complex z) {
+        // Construct a complex number object from another complex number object.
         this.re = z.re;
         this.im = z.im;
     }
 
     public double getRe() {
+        // Returns the real coefficient of the complex number.
         return re;
     }
 
     public double getIm() {
+        // Returns the imaginary coefficient of the complex number.
         return im;
     }
 
     public double abs () {
-        // abs(a + b i) = sqrt(a^2 + b^2).
+        // Returns the absolute value of the complex number using the expression abs(a + b i) = sqrt(a^2 + b^2).
         return Math.sqrt(Math.pow(this.re, 2.0) + Math.pow(this.im, 2.0) );
     }
 
     public Complex plus (Complex other) {
-        // (a + b i) + (c + d i) = (a + c) + (b + d) i
+        // Returns the summation of the complex number and another complex number taken as the argument.
         return new Complex(this.re + other.re, this.im + other.im);
     }
 
     public Complex times (Complex other) {
-        // (a + b i) * (c + d i) = (ac - bd) * (bc + ad) i
+        // Returns the summation of the complex number and another complex number taken as the argument.
         double t_re = this.re * other.re - this.im * other.im;
         double t_im = this.im * other.re + this.re * other.im;
         return new Complex(t_re, t_im);
@@ -42,6 +47,7 @@ public class Complex {
 
     @Override
     public String toString() {
+        // Returns the complex number represented in cartesian form.
         return this.re + " + " + this.im + " i";
     }
 }
--- Mandelbrot.java
+++ Mandelbrot.java
@@ -2,22 +2,57 @@ import java.util.Arrays;
 
 public class Mandelbrot {
     private static final int MAX = 20;
+    // Constant class field to determine how many itterations each point should make, higher value = higher accuracy and longer process time.
+
+    private static final int GRIDSIZE = 5;
+    // Constant class field determining how many fields the < sidelength > by < sidelength > matrix should be split into. 
+
+    private static int sidelength;
+    // Class field for the sidelength of the fractal.
+
+    private static Complex center;
+    // Class field for the center-point of the fractal in the complex number plane.
+
+    private static Complex[][] Grid = new Complex[GRIDSIZE][GRIDSIZE];
+    // Class field for the twodeimgrid of complex numbers found within the < sidelength > by < sidelength > matrix.
+
 
     public static void main(String[] args) {
         int[] args_int = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
+        // Convert type String[] args to type int[] args_int.
+
         Complex center = new Complex(args_int[0], args_int[1]);
+        // Define a complex number which acts as the fractal's center-point in the complex number plane.
+
         int sidelength = args_int[2];
+        // Define a sidelength to generate the fractal.
+
+        
+    }
+
+    private static Complex[] determineMatrixCoordinates
+
+    private static void draw (Complex[] C) {
+
     }
 
     private static int iterate (Complex z0) {
         Complex z = new Complex(z0);
+        // Clone the complex number argument z0 and store it in the variable z.
+
         for (int i = 0; i < MAX; i++) {
+        // Iterate for i going from 0 to the class field MAX. 
+
             if (z.abs() > 2.0) {
                 return i;
+                // If the absolute value of the complex number, i.e. the distance between z0 and the origin (0, 0) is greater than 2, the function returns the current number of iterations.
             }
+
             z = z.times(z).plus(z0);
+            // Otherwise the number z is set equal to z^2 plus the original argument z0.
         }
 
         return MAX;
+        // If the absolute value never exceeds 2.0, the loop will have been iterated MAX times, and as MAX is returned.
     }
 }
