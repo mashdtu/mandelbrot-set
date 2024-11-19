bdb1e7817e5d257e641d0e8e6fb02c4df2067238
mzshd <mashak8dk@gmail.com>
2024-11-19 22:09:24 +0100

    Working prototype

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -1,39 +1,80 @@
 import java.util.Arrays;
 
 public class Mandelbrot {
-    private static final int MAX = 20;
+    private static final int MAX = 250;
     // Constant class field to determine how many itterations each point should make, higher value = higher accuracy and longer process time.
 
-    private static final int GRIDSIZE = 5;
+    private static final int GRIDSIZE = 100;
     // Constant class field determining how many fields the < sidelength > by < sidelength > matrix should be split into. 
 
-    private static int sidelength;
+    private static double sidelength;
     // Class field for the sidelength of the fractal.
 
     private static Complex center;
     // Class field for the center-point of the fractal in the complex number plane.
 
     private static Complex[][] Grid = new Complex[GRIDSIZE][GRIDSIZE];
-    // Class field for the twodeimgrid of complex numbers found within the < sidelength > by < sidelength > matrix.
+    // Class field for the twodimensional grid of complex numbers found within the < sidelength > by < sidelength > matrix.
 
 
     public static void main(String[] args) {
-        int[] args_int = Arrays.stream(args).mapToInt(Integer::parseInt).toArray();
-        // Convert type String[] args to type int[] args_int.
+        double[] args_double = Arrays.stream(args).mapToDouble(Double::parseDouble).toArray();
+        // Convert type String[] args to type double[] args_double.
 
-        Complex center = new Complex(args_int[0], args_int[1]);
+        center = new Complex(args_double[0], args_double[1]);
         // Define a complex number which acts as the fractal's center-point in the complex number plane.
 
-        int sidelength = args_int[2];
+        sidelength = args_double[2];
         // Define a sidelength to generate the fractal.
 
-        
+        StdDraw.setXscale(-1, GRIDSIZE);
+        StdDraw.setYscale(-1, GRIDSIZE);
+        StdDraw.setPenRadius(0.8/(double)GRIDSIZE);
+        // Set dimensions and pen radius for the stdDraw library for use in visually representing the grid.
+
+        Grid = determineMatrixCoordinates();
+        // Determine the grid using the determineMatrixCoordinates() method.
+
+        draw(Grid);
+        // Draw the points of the matrix which fall within the mandelbrot set, determined using the iterate() method.
     }
 
-    private static Complex[] determineMatrixCoordinates
+    private static Complex[][] determineMatrixCoordinates () {
+        Complex[][] coordinates = new Complex[GRIDSIZE][GRIDSIZE];
+        // Define a dummy grid < coordinates > of size GRIDSIZE * GRIDSIZE to write to.
 
-    private static void draw (Complex[] C) {
+        for (int x = 0; x < GRIDSIZE; x++) {
+            for (int y = 0; y < GRIDSIZE; y++) {
+                // Iterate (x, y) over a twodimensional plane of size GRIDSIZE * GRIDSIZE. 
 
+                double x0 = center.getRe() - (double) sidelength / 2.0 + (double) (sidelength * x) / ((double) GRIDSIZE - 1);
+                double y0 = center.getIm() - (double) sidelength / 2.0 + (double) (sidelength * y) / ((double) GRIDSIZE - 1);
+                // Use expression (1) as described in the documentation to determine the coordinates (x0 and y0) of the center-point of the specific (x, y) sub-grid.
+
+                
+                coordinates[x][y] = new Complex(x0, y0);
+                // Write the value of the calculated coordinates to the grid as a complex number object.
+            }
+        }
+
+        return coordinates;
+        // Return the new rewritten dummy variable < coordinates >.
+    }
+
+    private static void draw (Complex[][] G) {
+        StdDraw.clear();
+        // Clear any preexisting objects on the StdDraw canvas.
+
+        for (int x = 0; x < GRIDSIZE; x++) {
+            for (int y = 0; y < GRIDSIZE; y++) {
+                // Iterate (x, y) over a twodimensional plane of size GRIDSIZE * GRIDSIZE. 
+
+                if (iterate(G[x][y]) == MAX) {
+                    StdDraw.point(x, y);
+                    // If the iterate method returns MAX, the complex number likely falls within the mandelbrot set, as such it is drawn on the canvas.
+                }
+            }
+        }
     }
 
     private static int iterate (Complex z0) {
